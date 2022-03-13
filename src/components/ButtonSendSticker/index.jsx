133edc0fede35dd2f8f44ec7c0dfdeb78384a98c
Patrick Asafe <patrickasafe@gmail.com>
2022-03-13 18:09:51 -0300

    new stickers button

--- src/components/ButtonSendSticker/index.jsx
+++ src/components/ButtonSendSticker/index.jsx
@@ -1,61 +1,75 @@
-import React from 'react';
-import { Box, Button, Text, Image } from '@skynexui/components';
-import appConfig from '../../../config.json';
+import React from "react";
+import { Box, Button, Text, Image } from "@skynexui/components";
+import appConfig from "../../../config.json";
 
 export function ButtonSendSticker(props) {
-  const [isOpen, setOpenState] = React.useState('');
+  const [isOpen, setOpenState] = React.useState("");
 
   return (
     <Box
       styleSheet={{
-        position: 'relative',
+        position: "relative",
       }}
     >
       <Button
         styleSheet={{
-          borderRadius: '50%',
-          padding: '0 3px 0 0',
-          minWidth: '50px',
-          minHeight: '50px',
-          fontSize: '20px',
-          marginBottom: '8px',
-          lineHeight: '0',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.neutrals[300],
-          filter: isOpen ? 'grayscale(0)' : 'grayscale(1)',
+          borderRadius: "50%",
+          padding: "0 0 0 0",
+          minWidth: "50px",
+          minHeight: "50px",
+          fontSize: "20px",
+          marginBottom: "8px",
+          lineHeight: "0",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          focus: { backgroundColor: appConfig.theme.colors.primary[500] },
+          backgroundColor: appConfig.theme.colors.primary[500],
+          filter: isOpen ? "grayscale(0)" : "grayscale(1)",
           hover: {
-            filter: 'grayscale(0)',
-          }
+            backgroundColor: appConfig.theme.colors.primary[500],
+            filter: "grayscale(0)",
+          },
+
         }}
-        label="😋"
+        label={
+          <Image
+            styleSheet={{
+              maxWidth: "36px",display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            src="/icons/man.png"
+            alt=''
+          ></Image>
+        }
         onClick={() => setOpenState(!isOpen)}
       />
       {isOpen && (
         <Box
           styleSheet={{
-            display: 'flex',
-            flexDirection: 'column',
-            borderRadius: '5px',
-            position: 'absolute',
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: "5px",
+            position: "absolute",
             backgroundColor: appConfig.theme.colors.neutrals[800],
             width: {
-              xs: '200px',
-              sm: '290px',
+              xs: "200px",
+              sm: "290px",
             },
-            height: '300px',
-            right: '30px',
-            bottom: '30px',
-            padding: '16px',
-            boxShadow: 'rgba(4, 4, 5, 0.15) 0px 0px 0px 1px, rgba(0, 0, 0, 0.24) 0px 8px 16px 0px',
+            height: "300px",
+            right: "30px",
+            bottom: "30px",
+            padding: "16px",
+            boxShadow:
+              "rgba(4, 4, 5, 0.15) 0px 0px 0px 1px, rgba(0, 0, 0, 0.24) 0px 8px 16px 0px",
           }}
           onClick={() => setOpenState(false)}
         >
           <Text
             styleSheet={{
               color: appConfig.theme.colors.neutrals["000"],
-              fontWeight: 'bold',
+              fontWeight: "bold",
             }}
           >
             Stickers
@@ -63,12 +77,12 @@ export function ButtonSendSticker(props) {
           <Box
             tag="ul"
             styleSheet={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
               flex: 1,
-              paddingTop: '16px',
-              overflow: 'scroll',
+              paddingTop: "16px",
+              overflow: "scroll",
             }}
           >
             {appConfig.stickers.map((sticker) => (
@@ -79,17 +93,18 @@ export function ButtonSendSticker(props) {
                     props.onStickerClick(sticker);
                   }
                 }}
-                tag="li" key={sticker}
+                tag="li"
+                key={sticker}
                 styleSheet={{
-                  width: '50%',
-                  borderRadius: '5px',
-                  padding: '10px',
+                  width: "50%",
+                  borderRadius: "5px",
+                  padding: "10px",
                   focus: {
                     backgroundColor: appConfig.theme.colors.neutrals[600],
                   },
                   hover: {
                     backgroundColor: appConfig.theme.colors.neutrals[600],
-                  }
+                  },
                 }}
               >
                 <Image src={sticker} />
@@ -99,5 +114,5 @@ export function ButtonSendSticker(props) {
         </Box>
       )}
     </Box>
-  )
+  );
 }
